e22c1ad3ac8649594be89f881203d27f48a5a083
SergioXXI <94912678+SergioXXI@users.noreply.github.com>
2021-12-25 11:30:44 +0100

    Update raiz.c

--- raiz.c
+++ raiz.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TRUE 1
+#define FALSE 0
+
+
 
 
 /*
@@ -25,8 +29,101 @@ Vamos a hacer lo siguiente hasta que el array de pares este vacio, es decir, est
 */
 
 // Divide un numero en pareja de dos digitos y los guarda en un array
-int dividir_parejas(int numero, ){
-    int numero_parejas;
+
+int es_par(int numero){
+
+  int cont=0;
+
+  while (numero > 0)
+    {
+      numero = numero / 10;
+      cont++;
+    }
+
+    if (cont % 2 != 0){
+      return FALSE;
+    }else
+    {
+      return TRUE;
+    }
+}
+
+unsigned concatenate(unsigned x, unsigned y) {
+    unsigned pow = 10;
+    while(y >= pow)
+        pow *= 10;
+    return x * pow + y;        
+}
+
+//Calcula en cuantas parejas de dos se tiene que dividir el numero
+int calculo_array(int numero)
+{
+    int numero_parejas=0;
+
+    int numero_par=es_par(numero);
+
+    while (numero > 0)
+        {
+            /*
+            printf ("\n--%d--\n",numero); 
+            printf ("\n--%d--\n",numero_parejas );
+            printf ("\n--%d--\n",es_par(numero) );
+            */
+            
+            if((numero/100)<0 && numero_par==FALSE)
+     
+            {
+                numero = numero / 10;
+                numero_parejas++;
+            }
+
+        numero = numero / 100;
+        numero_parejas++;
+
+    }
+
+    return numero_parejas;
+}
+
+
+//divide el numero de parejas de dos y lo inserta en un array auxiliar para despues darles las vuelta
+int dividir_parejas(int numero, int *numero_array,int tamano_array){
+    
+    int numero_parejas=0;
+
+    int array_aux[tamano_array];
+
+    int numero_par=es_par(numero);
+    
+
+    while (numero > 0)
+        {
+            /*
+            printf ("\n--%d--\n",numero); 
+            printf ("\n--%d--\n",numero_parejas );
+            printf ("\n--%d--\n",es_par(numero) );
+            */
+            
+            if((numero/100)<0 && numero_par==FALSE)
+     
+            {
+
+                array_aux[numero_parejas] = numero % 10;
+                numero = numero / 10;
+                numero_parejas++;
+            }
+        array_aux[numero_parejas] = numero % 100;
+        numero = numero / 100;
+        numero_parejas++;
+
+
+    }
+
+
+    for(int s=numero_parejas-1,i=0;s>=0;s--,i++)
+    {
+        numero_array[i]=array_aux[s];
+    }
 
     return numero_parejas;
 }
@@ -35,58 +132,71 @@ int busca_cercano(int numero){
     int i;
     for(i=0; i<numero; i++){
         if((i*i) >= numero){
+            printf ("\n!!!\n");
+            printf ("\n%d\n",i);
             return i-1;
         }
+            printf ("\n%d\n",i);
     }
+    printf("No devuelve nada");  
 }
 
+//Funcion que calcula el cuadrado de un numero
 int calcula_cuadrado(int n){
     return n*n;
 }
 
+
 int busca_n(int doble_solucion, int resto){
     int i, concatenacion;
-    for(i=1; i=<resto; i++){
-        concatenacion = strcat(doble_solucion, i);
+    for(i=1; i<=resto; i++){
+        concatenacion = concatenate(doble_solucion, i);
         if((concatenacion *i) > resto){
             return i-1;
-        } )
+        } 
     }
 
 }
 
 
-int calcula_raiz(int numero, int *numero_array, *resto, *solucion){
+int calcula_raiz(int numero, int *numero_array, int *resto, int *solucion, int tamano_array){
 
     int i=0, raiz_cercana, numero_parejas, continuar = 1, cuadrado, doble_solucion, n;
-
-    //Dividimos al numero en parejas de dos empezando por la izquierda, lo guardamos en un array
-    numero_parejas = dividir_parejas(numero, numero_array, resto, solucion);    //1
-
-    //Vamos a empezar a calcular, buscamos el numero que multiplicado por si mismo nos de el numero mas parecido al primero del array y lo guardamos en la solucion
-    resto = numero_array[0]
-    solucion = busca_cercano(resto);          //2
-
     
 
+    //Dividimos al numero en parejas de dos empezando por la izquierda, lo guardamos en un array
+    numero_parejas = dividir_parejas(numero, numero_array,tamano_array);    //1
+    //Vamos a empezar a calcular, buscamos el numero que multiplicado por si mismo nos de el numero mas parecido al primero del array y lo guardamos en la solucion
+    (*resto) = numero_array[0];
+    
+    printf ("\n!!!\n");
 
-        //Hacemos el cuadrado de la solucion para restarselo al numero que hemos sacado del array
-        cuadrado = calcula_cuadrado(solucion);          //3
-        resto = resto - cuadrado;             //4
+    (*solucion) = busca_cercano((*resto));          //2
+    
+    printf ("\n!!!\n");
+
+
+
+    //Hacemos el cuadrado de la solucion para restarselo al numero que hemos sacado del array
+    cuadrado = calcula_cuadrado((*solucion));          //3
+    
+   
+    
+    (*resto) = (*resto) - cuadrado;             //4
 
 
     while(continuar){
 
         // Con el doble_solucion de la solucino nos vamos a la parte auxiliar 
-        doble_solucion = solucion *2;                            //4
+        doble_solucion = (*solucion) *2;                            //4
         // Concatenamos la siguiente pareja de numeros
-        resto = strcat(resto, numero_array[i+1]);     //5
+        (*resto) = concatenate((*resto), numero_array[i+1]);     //5
         //Buscamos un numero que sea de la forma solucion*2 _ x_ menor que el resto
-        n = busca_n(doble_solucion, resto);                          //6
+        n = busca_n(doble_solucion, (*resto));                          //6
         // Concatenamos el numero encontrado con la solucino
-        solucion = strcat(solucion, n);                          //7
+        (*solucion) = concatenate( (*solucion), n);                          //7
         // Le restamos al resto doble_solucion concatenado con n x n
-        resto = resto - (strcat(doble_solucion, n)*n);                    
+        (*resto) = (*resto) - (concatenate(doble_solucion, n)*n);                    
 
 
         
@@ -103,25 +213,47 @@ int calcula_raiz(int numero, int *numero_array, *resto, *solucion){
             continuar = 0;
         }
     }
-    
-
 
 
 }
 
 
 void mostrar_raiz(int *resto, int *solucion){
-    printf("solucion: %d", resto);
-    printf("RESTO: %d", solucion);
+    printf("solucion: %d", (*resto));
+    printf("RESTO: %d", (*solucion));
+}
+
+int comprobar_num(int numero){
+
 }
 
 
-int main(int argc, char *argv[]){
+int main (int argc, char *argv[])
+{
 
-    int numero, *numero_array, *resto, *solucion;
+    int numero, *numero_array, *resto, *solucion, tamano_array;
+    
+    if (argc > 0)
+    {
+        
+        numero = atoi (argv[1]);
+        if (numero <= 0)
+        {
+            fprintf (stderr, "No existen raices negativas dentro de los numeros reales.\n");
+            return 1;
+        }
+	}
+    else{
+        fprintf (stderr, "Error: No se han introducido todos los datos.\n");
+        return 1;
+    }
+
+    tamano_array=calculo_array(numero);
+    numero_array=(int *) calloc (tamano_array, sizeof (int));
+    
+    calcula_raiz(numero, numero_array, resto, solucion, tamano_array);
+    
 
 
-    calcula_raiz(numero, numero_array, resto, solucion);
-
-    mostrar_solucion(resto, solucion);
+   mostrar_raiz(resto, solucion);
 }
